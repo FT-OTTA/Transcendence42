8f00118f5cb6964705f2d76811bcd156b3033041
tcoeffet <teddy.coeffet@gmail.com>
2026-05-15 15:01:07 +0200

    fixed a few visual issues, add a custom scrollbar, and implemented match history display

--- front/app/components/navigation/Navbar.tsx
+++ front/app/components/navigation/Navbar.tsx
@@ -6,17 +6,17 @@ import MobileNavbar from "./MobileNavbar"
 
 const NavbarClassDesktop = clsx
 (
-	"opacity-0 transition duration-300 hover:opacity-100",
+	"opacity-55 transition duration-300 hover:opacity-100",
 	"flex absolute top-0 left-0 w-full items-center justify-center",
-	"bg-black/25 backdrop-blur-sm"
+	"bg-black/25 backdrop-blur-sm",
 )
 
 const NavLine = clsx
 (
-	"border border-grey-500",
+	"border border-grey-900",
 	"h-[1px] w-[200px]",
 	"absolute top-12 left-1/2 -translate-x-1/2",
-	"bg-white/25"
+	"bg-white/15"
 )
 
 export default function Navbar()
@@ -25,9 +25,9 @@ export default function Navbar()
 		<div>
 			<div className="hidden md:flex">
 				<nav className={ NavbarClassDesktop }>
-					<NavItem text="Home" href="/" />
-					<NavItem text="Community" href="/community" />
-					<NavItem text="Lobby" href="/lobby" />
+					<NavItem text="home" href="/" />
+					<NavItem text="community" href="/community" />
+					<NavItem text="lobby" href="/lobby" />
 				</nav>
 				<div className={ NavLine }/>
 			</div>

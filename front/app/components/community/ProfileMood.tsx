903813172e7bec258ce36c8306747d488871f265
tcoeffet <teddy.coeffet@gmail.com>
2026-05-23 21:25:05 +0200

    finished to setup the moodphrase and to make it editable on the user s page

--- front/app/components/community/ProfileMood.tsx
+++ front/app/components/community/ProfileMood.tsx
@@ -1,39 +1,80 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Pencil } from "lucide-react";
 
-export default function ProfileMood({
-	login,
-	initialMood,
-}: {
-	login: string;
+type Props = {
 	initialMood: string;
-}) {
-	console.log("INSIDE : login = ", login)
-	const [mood, setMood] = useState(initialMood);
+	username: string;
+};
+
+export default function ProfileMood({ initialMood, username }: Props)
+{
+	const [currentMood, setCurrentMood] = useState(initialMood);
 	const [isOwner, setIsOwner] = useState(false);
 	const [loading, setLoading] = useState(true);
+	const [editing, setEditing] = useState(false);
 
 	useEffect(() => {
 		const storedUsername = localStorage.getItem("username");
 
-		setIsOwner(storedUsername === login);
+		setIsOwner(storedUsername === username);
 		setLoading(false);
-	}, [login]);
+	}, [username]);
+	
+	async function saveMood(newMood: string) {
+	const res =	await fetch(`http://localhost:3000/users/${username}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				moodphrase: newMood,
+			}),
+		});
+	}
+
 
 	if (loading) {
 		return <p className="text-white/40 italic">Loading...</p>;
 	}
 
 	if (!isOwner) {
-		return <p className="text-white/60 italic">{mood}</p>;
+		return <p className="text-white/60 italic">{ currentMood }</p>;
 	}
 
-	return (
-		<input
-			value={mood}
-			onChange={(e) => setMood(e.target.value)}
-			className="bg-black/30 border border-sky-500 text-white px-2 py-1 rounded"
-		/>
+	return editing ?(
+			<input
+				autoFocus
+				value={currentMood}
+				onChange={(e) => setCurrentMood(e.target.value)}
+				onBlur={() => saveMood(currentMood)}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						saveMood(currentMood);
+						setEditing(false);
+					}
+				}}
+				onBlur={() => {
+					saveMood(currentMood);
+					setEditing(false);
+				}}
+				className="bg-black/30 border border-sky-500 text-white px-2 py-1 rounded"
+			/>
+	) : (
+		<div className="flex gap-2 items-center">
+
+			<Pencil
+				size={14}
+				className="text-sky-400"
+				/>
+			
+			<p 
+				className="text-white/60 italic cursor-pointer hover:text-white transition whitespace-normal break-words max-w-xs md:max-w-sm"
+				onClick={() => setEditing(true)}
+				>
+				{currentMood || "Click to set your mood"}
+			</p>
+		</div>
 	);
 }

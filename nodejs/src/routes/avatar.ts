d87d5a2f3e1af0d2034f57ac5a17176e6ee6cb70
tcoeffet <teddy.coeffet@gmail.com>
2026-05-25 18:48:01 +0200

    add a route to post a avatar for a user and an entry in the db to put avatar path. Avatar is stored in databases/users/avatars/[login].png

---
+++ nodejs/src/routes/avatar.ts
@@ -0,0 +1,49 @@
+import { Router } from 'express'
+import { prisma } from '../../prisma/prisma.ts'
+import path from "path";
+import { fileURLToPath } from 'url';
+import fs from "fs";
+import sharp from "sharp";
+
+const router = Router()
+
+router.post('/:login/avatar', async (req, res) => {
+	try 
+	{
+		const { login } = req.params;
+		const file = req.files?.avatar;
+		const __filename = fileURLToPath(import.meta.url);
+		const __dirname = path.dirname(__filename);
+		console.log("FILES:", req.files);
+
+		if (!file)
+		{
+			return res.status(400).json({error : "No file uploaded" });
+		}
+
+		const filename = `${login}.png`;
+		const folder = path.join(process.cwd(), "databases/users/avatars");
+
+		fs.mkdirSync( folder, { recursive: true });
+
+		const outputPath = path.join(folder, filename);
+
+		await sharp(file.data)
+			.resize(128,128)
+			.png()
+			.toFile(outputPath);
+
+		await prisma.user.update({
+			where: {username: login },
+			data: {avatarUrl: `/avatars/${filename}`},
+		});
+	} 
+	catch (err)
+	{
+		console.error("UPLOAD ERROR:", err);
+		return res.status(500).json({ error: "upload failed" });
+	}
+	return res.json({ ok: true});
+});
+
+export default router;

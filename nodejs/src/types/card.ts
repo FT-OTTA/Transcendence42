5b389dd8b10d89ecc3703a1a3616cba23361c106
rat <rat@gmail.com>
2026-06-02 18:16:35 +0200

    started to translate cards and spells

--- nodejs/src/types/card.ts
+++ nodejs/src/types/card.ts
@@ -1,35 +1,40 @@
-import type { Zone, BfZone } from './zones.ts'
-import type { EffectType, EffectTarget, EffectTime, TargetType, Effect } from './effects.ts'
+import type { Zone } from './zones.ts'
+import type { EffectTime, Effect } from './effects.ts'
 import type { Hero } from './hero.ts'
 
 export type CardType = "creature" | "spell" | "building";
-
 export type CreatureState = "sick" | "ready";
-
 export type CardClass = "common" | "Warrior" | "Druid";
 
 export type Card = {
     kind: "card";
     zone: Zone;
-    effect: string; // JSON string des effets, à parser côté client
+    effect: string; 
     effects: Effect[];
     timing: EffectTime;
     type: CardType;
     class: CardClass;
     owner: Hero;
     state: CreatureState;
-    runeCost: number;
     idInCollection: number;
     idInGame: number;
     baseForce: number;
     currForce: number;
     baseEndurance: number;
     currEndurance: number;
-    cardName: string;
-    effectText: string;
-    fullPicPath: string;
-    smallPicPath: string;
-    cardBackPath: string;
+    illustration: string;
+    
+    rune_cost: number;
+    
+    name_en: string;
+    type_en: string;
+    effect_text_en: string;
+
+    name_fr?: string;
+    type_fr?: string;
+    effect_text_fr?: string;
+
+    [key: string]: any; 
 };
 
 // export type PlayCardPayload = {

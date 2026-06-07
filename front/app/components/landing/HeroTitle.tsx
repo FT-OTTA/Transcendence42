71f6d2566131f96bc5c998103da7b814ab63e86a
Beaumanoir <theo.beaumanoir@gmail.com>
2026-06-07 02:54:33 +0200

    push clownesque du fix de la font qui fait un warning

--- front/app/components/landing/HeroTitle.tsx
+++ front/app/components/landing/HeroTitle.tsx
@@ -1,7 +1,6 @@
-import { nordic } from "../../fonts"
 import clsx from "clsx";
 
-const titleClassImg = clsx
+const titleRuneImg = clsx
 (
   "absolute inset-0",
   "bg-[url('/faded_rune.png')]",
@@ -10,27 +9,19 @@ const titleClassImg = clsx
   "drop-shadow-[0_0_40px_rgba(120,180,255,0.5)]" 
 );
 
-const titleClassTxt = clsx
-(
-  nordic.className,
-  "translate-x-1.5 md:translate-x-3",
-  "translate-y-2 md:translate-y-5",
-  "cursor-default select-none",
-  "text-8xl md:text-[12rem] lg:text-[13rem]",
-  "text-blue-200 drop-shadow-[0_0_20px_rgba(100,200,255,0.2)]"
-);
-
 export default function HeroTitle()
 {
-	return (
-		<div className="relative inline-block translate-y-25">
-			<div className={ titleClassImg } />
-			
-			<h1 className={ titleClassTxt }>
-				<span className="tracking-[0.1em]">OT</span>
-				<span className="tracking-[0.15em]">TA</span>
-			</h1>
+    return (
 
-		</div>
-	);
+    <div className="relative inline-block translate-y-25">
+            <div className={ titleRuneImg + " z-0 "} />
+
+      <img 
+      src="/otta_logo.svg"
+      draggable={ false }
+      
+      className="select-none pr-5 relative z-10 -sky-200 drop-shadow-[0_0_40px_rgba(120,180,255,0.5)]"
+      />
+        </div>
+    );
 }
